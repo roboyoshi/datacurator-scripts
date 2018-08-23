a183c07731f27307176d13552ecd238ffbb69c2c
roboyoshi <21063396+roboyoshi@users.noreply.github.com>
2018-08-23 15:39:58 +0200

    add "-o

--- unscene.sh
+++ unscene.sh
@@ -5,5 +5,5 @@
 # + ------------------------------- +
 
 DATA_DIR=/path/to/data
-find ${DATA_DIR} -type f -name '*.rar' -execdir unrar x {} \;
+find ${DATA_DIR} -type f -name '*.rar' -execdir unrar x -o+ {} \;
 find ${DATA_DIR} -type f -name '*.rar' -execdir sh -c 'rm "${0%%.rar}".r??' {} \;
